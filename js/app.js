af3dbf4eaacd9cc7821a8726bd8ffaa103dfa424
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-18 14:36:30 +0600

    UI update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+
+let income = document.getElementById('total-income');
+let foodCost = document.getElementById('food-cost');
+let fentCost = document.getElementById('rent-cost');
+let clothesCost = document.getElementById('cloths-cost');
+
+
+// let calculate = document.getElementById('calculate-btn');
+
+function calculate(){
+   
+    console.log('button click');
+}
